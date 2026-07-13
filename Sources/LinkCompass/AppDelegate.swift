4ae510eaced18495cd0a30d1d2d61d772dc12826
Reid Chatham <reid.chatham@gmail.com>
2026-07-12 23:42:38 -0700

    fix: remove duplicate menu bar item

--- Sources/LinkCompass/AppDelegate.swift
+++ Sources/LinkCompass/AppDelegate.swift
@@ -9,7 +9,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     private let onboardingWindowController = OnboardingWindowController()
     private let linkOpener = LinkOpener()
     private let ruleStore: RuleStore
-    private var fallbackStatusItem: NSStatusItem?
     private var openOnboardingObserver: NSObjectProtocol?
     private var pendingOnboardingLaunch: DispatchWorkItem?
     private var hasHandledIncomingURL = false
@@ -26,7 +25,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         ProcessInfo.processInfo.disableAutomaticTermination("LinkCompass keeps a menu bar item available for browser routing.")
-        configureFallbackStatusItem()
         openOnboardingObserver = NotificationCenter.default.addObserver(
             forName: .linkCompassOpenOnboarding,
             object: nil,
@@ -47,35 +45,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         false
-    }
-
-    private func configureFallbackStatusItem() {
-        let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-        statusItem.isVisible = true
-
-        if let button = statusItem.button {
-            button.title = ""
-            button.image = NSImage(systemSymbolName: "safari", accessibilityDescription: "LinkCompass")
-            button.image?.isTemplate = true
-            button.toolTip = "LinkCompass"
-        }
-
-        let menu = NSMenu()
-        menu.addItem(NSMenuItem(title: "Open LinkCompass…", action: #selector(openOnboardingFromMenu), keyEquivalent: ""))
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit LinkCompass", action: #selector(quitFromMenu), keyEquivalent: "q"))
-        menu.items.forEach { $0.target = self }
-        statusItem.menu = menu
-
-        fallbackStatusItem = statusItem
-    }
-
-    @objc private func openOnboardingFromMenu() {
-        showOnboarding()
-    }
-
-    @objc private func quitFromMenu() {
-        NSApp.terminate(nil)
     }
 
     func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
